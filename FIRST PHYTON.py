7a1112ff1ff1c2ed248d56870e9b48054eebefe9
amarashnl <143410995+amarashnl@users.noreply.github.com>
2023-11-18 19:54:18 +0800

    Add files via upload

---
+++ FIRST PHYTON.py
@@ -0,0 +1,12 @@
+#THIS PROGRAM COMPUTES THE INPUT GIVEN BY THE USER 
+
+print("Enter first number: ", end=" ")
+num1 = int(input ())                          #ACCEPTING FIRST INPUT
+print("Enter Second Number: ", end=" ")
+num2= int(input ())                           #ACCEPTING SECOND INPUT
+
+result = num1 * num2       #COMPUTING THE RESULT
+
+print ("Answer is:",result)              #PRINTING THE RESULT
+
+
